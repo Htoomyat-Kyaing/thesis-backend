ebb4e95b27f8d004a054e31401a1407d5c5530a7
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2024-05-31 13:58:27 +0630

    signup with jwt and cookies

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/customError.js";
+import jwt from "jsonwebtoken";
 
 // include next for middleware
 export const signup = async (req, res, next) => {
@@ -13,5 +14,26 @@ export const signup = async (req, res, next) => {
     res.status(201).json("User created successfully");
   } catch (err) {
     next(errorHandler(err.statusCode, err.message));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+    const { password: pw, ...userInfo } = validUser._doc;
+    return res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 60 * 60),
+      })
+      .status(200)
+      .json(userInfo);
+  } catch (err) {
+    next(err);
   }
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,10 @@
 import e from "express";
 import { signup } from "../controllers/auth.controller.js";
+import { signin } from "../controllers/auth.controller.js";
 
 const router = e.Router();
 
 router.post("/sign-up", signup);
+router.post("/sign-in", signin);
 
 export default router;
