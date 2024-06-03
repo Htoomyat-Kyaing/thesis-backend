ba5e2962299376543d30beb399ca81240e56e613
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2024-06-03 15:40:11 +0630

    test 2

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use("/auth", authRouter);
 
 app.use(express.static(path.join(__dirname, "/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "/dist", "index.html"));
 });
 
 //middleware
