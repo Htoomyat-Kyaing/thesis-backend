447041a6560081dbae6a82640fe833d3e87696cf
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2024-06-15 16:24:37 +0630

    getItem + getUser routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -31,6 +31,20 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
+export const getUser = async (req, res, next) => {
+  // if (req.user.id !== req.params.id)
+  //   return next(errorHandler(401, "You can only update your account"));
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) res.status(404).json("User not found");
+
+    const { password, ...userInfo } = user._doc;
+    res.status(200).json(userInfo);
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const getUserItems = async (req, res, next) => {
   if (req.user.id === req.params.id) {
     try {
--- routes/item.route.js
+++ routes/item.route.js
@@ -3,6 +3,7 @@ import { verifyToken } from "../utils/verifyToken.js";
 
 import {
   createItem,
+  getItem,
   removeItem,
   updateItem,
 } from "../controllers/item.controller.js";
@@ -12,6 +13,6 @@ const router = e.Router();
 router.post("/create", verifyToken, createItem);
 router.delete("/delete/:id", verifyToken, removeItem);
 router.patch("/update/:id", verifyToken, updateItem);
-router.get("/:id", verifyToken, updateItem);
+router.get("/:id", getItem);
 
 export default router;
