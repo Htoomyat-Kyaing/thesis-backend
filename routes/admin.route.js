40902e07b4ebd16c586a8d54853de1b643cc800f
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2024-08-04 23:22:54 +0630

    manage user functionaliy

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -77,4 +77,34 @@ router.patch("/update-item/:itemId", checkRole, async (req, res, next) => {
   }
 });
 
+router.post("/edit-user/:id", async (req, res, next) => {
+  // res.status(200).json("this is edit user by admin api");
+  // if (req.user.id !== req.params.id)
+  //   return next(errorHandler(401, "You can only update your account"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+          role: "user",
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...userInfo } = updateUser._doc;
+    res.status(200).json(userInfo);
+  } catch (error) {
+    next(error);
+  }
+});
+
 export default router;
